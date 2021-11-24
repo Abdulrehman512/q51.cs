bc02ffe14be4b8d9fe837659fd925b29c37aa39c
AbdulRehman <abdulrehman11992@gmail.com>
2021-11-23 22:40:15 -0800

    last commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace q51.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(1 + 8/2 + ((1*4) + (5*4))/4);
+            Console.WriteLine((1+1+1+1)/2 + (1+1+1)/3);
+            Console.WriteLine(5*5+5/5+6);
+            Console.WriteLine(((3+4)+(4*7))/5);
+
+        }
+    }
+}
